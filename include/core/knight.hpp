d0b17d4d28af4a628ea30d59eeecd4cfcc20e8ee
CoderEx24 <coderex24@protonmail.com>
2021-11-08 14:26:34 +0200

    added Knight declaration

---
+++ include/core/knight.hpp
@@ -0,0 +1,16 @@
+#ifndef KNIGHT_H
+#define KNIGHT_H
+
+#include "abstract_chess_piece.hpp"
+
+class Knight : public AbstractChessPiece
+{
+	public:
+		Knight(): AbstractChessPiece() {}
+		Knight(int x, int y, PieceColor c): AbstractChessPiece(x, y, c) {}
+
+		std::vector<Position> get_valid_positions(bool **occupied_positions) override;
+};
+
+#endif
+
