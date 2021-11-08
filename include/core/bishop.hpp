536c582768d6ce1f7942af9460e8c5ae5e2cf971
CoderEx24 <coderex24@protonmail.com>
2021-11-08 14:22:55 +0200

    added Bishop declaration

---
+++ include/core/bishop.hpp
@@ -0,0 +1,17 @@
+#ifndef BISHOP_H
+#define BISHOP_H
+
+#include "abstract_chess_piece.hpp"
+
+class Bishop : public AbstractChessPiece
+{
+	public:
+		Bishop(): AbstractChessPiece() {}
+		Bishop(int x, int y, PieceColor c): AbstractChessPiece(x, y, c) {}
+
+		std::vector<Position> get_valid_positions(bool **occupied_positions) override;
+		
+};
+
+#endif
+
