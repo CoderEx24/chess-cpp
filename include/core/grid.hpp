dfe97c1aa594d42e5be7d334df43118c37265464
CoderEx24 <coderex24@protonmail.com>
2021-11-11 13:49:45 +0200

    some refactoring

--- include/core/grid.hpp
+++ include/core/grid.hpp
@@ -9,9 +9,11 @@
 #include "core/queen.hpp"
 #include "core/king.hpp"
 
+
 class Grid
 {
 	AbstractChessPiece *grid[8][8];
+	std::vector<FakeChessPiece*> fake_pieces_list;
 	bool occupied_positions[8][8], game_over;
 	PieceColor current_turn, winner;
 	King *threatened_king, *white_king, *black_king;
@@ -23,9 +25,10 @@ class Grid
 	public:
 		Grid(): current_turn(WHITE), threatened_king(nullptr) { init_grid(); }
 		Grid(PieceColor turn): current_turn(turn), threatened_king(nullptr) { init_grid(); }
-		
-		bool is_game_over() const { return this->game_over; }
-		PieceColor get_winner() const { return this->winner; }
+
+		bool is_game_over() const { return game_over; }
+		PieceColor get_winner() const { return winner; }
+		const std::vector<FakeChessPiece*> get_fake_piece_list() const { return fake_pieces_list; }
 
 		void move(const Position& piece, const Position& dest);
 		const std::vector<Position>& get_possible_moves(const Position& piece);
