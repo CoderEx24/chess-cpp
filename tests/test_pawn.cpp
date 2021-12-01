32d271ee3338c504a00fb5c8cdf84513393668a6
CoderEx24 <coderex24@protonmail.com>
2021-12-01 21:44:55 +0200

    rewrote the pawn test suite

--- tests/test_pawn.cpp
+++ tests/test_pawn.cpp
@@ -9,85 +9,69 @@ class DummyPawn : public Pawn
 		bool get_is_first_move() const { return is_first_move; }
 };
 
-TEST_GROUP(TestWhitePawn)
+TEST_GROUP(TestPawn)
 {
-	bool occupied_positions[8][8] {
-		{ true, true, true, true, true, true, true, true},  
-		{ true, true, true, true, true, true, true, true}, 
-		{ false, false, false, false, false, false, false, false}, 
-		{ false, false, false, false, false, false, false, false}, 
-		{ false, false, false, false, false, false, false, false}, 
-		{ false, false, false, false, false, false, false, false}, 
-		{ true, true, true, true, true, true, true, true},  
-		{ true, true, true, true, true, true, true, true},  
+	PieceColor occupied_positions[8][8] {
+		{BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+		{BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+		{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+		{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+		{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+		{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+		{WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE},
+		{WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE},
 	};
 
 	TEST_SETUP();
 	TEST_TEARDOWN();
 };
 
-TEST_GROUP(TestBlackPawn)
+TEST(TestPawn, test_pawn_movement_before_first_move)
 {
-	bool occupied_positions[8][8] {
-		{ true, true, true, true, true, true, true, true},  
-		{ true, true, true, true, true, true, true, true}, 
-		{ false, false, false, false, false, false, false, false}, 
-		{ false, false, false, false, false, false, false, false}, 
-		{ false, false, false, false, false, false, false, false}, 
-		{ false, false, false, false, false, false, false, false}, 
-		{ true, true, true, true, true, true, true, true},  
-		{ true, true, true, true, true, true, true, true},  
-	};
+	// Testing white pawn
+	DummyPawn white_pawn(6, 0);
 
-	TEST_SETUP();
-	TEST_TEARDOWN();
-};
-
-TEST(TestWhitePawn, test_pawn_movement_before_first_move)
-{
-	DummyPawn pawn(6, 0);
-
-	CHECK(pawn.get_is_first_move());
+	CHECK(white_pawn.get_is_first_move());
 	
-	std::vector<Position> possible_moves(pawn.get_valid_positions((bool**) occupied_positions));
-	CHECK_EQUAL(possible_moves.size(), 2);
+	std::vector<Position> possible_moves(white_pawn.get_valid_positions((FakeGrid) occupied_positions));
+	CHECK(possible_moves.size() == 2);
 	CHECK(possible_moves[0] == Position(5, 0));
-	CHECK(possible_moves[1] == Position(4, 0));	
+	CHECK(possible_moves[1] == Position(4, 0));
+
+	// Testing black pawn
+	DummyPawn black_pawn(1, 0, BLACK);
+
+	CHECK(black_pawn.get_is_first_move());
+
+	possible_moves = black_pawn.get_valid_positions((FakeGrid) occupied_positions);
+	CHECK(possible_moves.size() == 2);
+	CHECK(possible_moves[0] == Position(2, 0));
+	CHECK(possible_moves[1] == Position(3, 0));
+
 }
 
-TEST(TestWhitePawn, test_pawn_movement_after_first_move)
+TEST(TestPawn, test_pawn_movement_after_first_move)
 {
+	// Testing white pawn
 	DummyPawn pawn(6, 0);
 	CHECK(pawn.get_is_first_move());
 
 	pawn.set_position(5, 0);
 	CHECK(!pawn.get_is_first_move());
 	
-	std::vector<Position> possible_moves(pawn.get_valid_positions((bool**) occupied_positions));
-	CHECK_EQUAL(possible_moves.size(), 1);
-	CHECK(possible_moves[0] == Position(4, 0));	
+	std::vector<Position> possible_moves(pawn.get_valid_positions((FakeGrid) occupied_positions));
+	CHECK(possible_moves.size() == 1);
+	CHECK(possible_moves[0] == Position(4, 0));
+
+	// Testing black pawn
+	DummyPawn black_pawn(1, 2, BLACK);
+	CHECK(black_pawn.get_is_first_move());
+
+	black_pawn.set_position(2, 2);
+	CHECK(!black_pawn.get_is_first_move());
+
+	possible_moves = black_pawn.get_valid_positions((FakeGrid) occupied_positions);
+	CHECK(possible_moves.size() == 1);
+	CHECK(possible_moves[0] == Position(2, 2));
 }
 
-TEST(TestBlackPawn, test_pawn_movement_before_first_move)
-{
-	DummyPawn pawn(1, 0);
-	CHECK(pawn.get_is_first_move());
-
-	std::vector<Position> possible_moves(pawn.get_valid_positions((bool**) occupied_positions));
-	CHECK_EQUAL(possible_moves.size(), 2);
-	CHECK(possible_moves[0] == Position(2, 0));
-	CHECK(possible_moves[1] == Position(3, 0));
-}
-
-TEST(TestBlackPawn, test_pawn_movement_after_first_move)
-{
-	DummyPawn pawn(1, 0);
-	CHECK(pawn.get_is_first_move());
-
-	pawn.set_position(2, 0);
-	CHECK(!pawn.get_is_first_move());
-
-	std::vector<Position> possible_moves(pawn.get_valid_positions((bool**) occupied_positions));
-	CHECK_EQUAL(possible_moves.size(), 1);
-	CHECK(possible_moves[0] == Position(3, 0));
-}
