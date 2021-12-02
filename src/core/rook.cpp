cab62e5f1847a9bb96963813a1e4165472245849
CoderEx24 <coderex24@protonmail.com>
2021-12-02 13:26:28 +0200

    added a dummy get_valid_positions

---
+++ src/core/rook.cpp
@@ -0,0 +1,8 @@
+#include "core/rook.hpp"
+
+const std::vector<Position>& Rook::get_valid_positions(FakeGrid occupied_positions)
+{
+	std::vector<Position> *poses = new std::vector<Position>(0);
+	return *poses;
+}
+
