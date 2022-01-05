6cc9800177236dbdd6409eaefdddcd61aa3bcf3a
CoderEx24 <coderex24@protonmail.com>
2022-01-05 16:21:18 +0200

    implemented knights movements, all tests passed

--- src/core/knight.cpp
+++ src/core/knight.cpp
@@ -2,8 +2,29 @@
 
 const std::vector<Position>& Knight::get_valid_positions(FakeGrid occupied_positions)
 {
-	std::vector<Position> *poses = new std::vector<Position>(0);
-	return *poses;
+	possible_moves.clear();
+
+	Position OFFSETS[8] {
+		Position(-2,   1),
+		Position(-2,  -1),
+		Position( 1,   2),
+		Position(-1,   2),
+		Position( 2,  -1),
+		Position( 2,   1),
+		Position(-1,  -2),
+		Position( 1,  -2),
+	};
+
+	Position point_to_add;
+	for (int i = 0; i < 8; i ++)
+	{
+		point_to_add = this->pos + OFFSETS[i];
+		if (in_bounds(point_to_add) &&
+				occupied_positions[point_to_add.x][point_to_add.y] != this->color)
+			possible_moves.push_back(point_to_add);
+	}
+
+	return possible_moves;
 }
 
 
