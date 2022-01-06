2f885710d0c3d4a5285a92a4e05254debb2d792e
CoderEx24 <coderex24@protonmail.com>
2022-01-07 01:40:03 +0200

    implemented rook, all tests passed

--- src/core/rook.cpp
+++ src/core/rook.cpp
@@ -2,7 +2,32 @@
 
 const std::vector<Position>& Rook::get_valid_positions(FakeGrid occupied_positions)
 {
-	std::vector<Position> *poses = new std::vector<Position>(0);
-	return *poses;
+	possible_moves.clear();
+
+	Position OFFSETS[4] { Position(-1, 0), Position(0, 1), Position(1, 0), Position(0, -1) };
+	Position point_to_add;
+	bool is_blocked[4] { false, false, false, false };
+	bool all_blocked = false;
+	int i = -1;
+
+	while (!all_blocked)
+	{
+		i ++;
+		if (is_blocked[i % 4]) continue;
+		
+		point_to_add = this->pos + OFFSETS[i % 4] * ((i / 4) + 1);
+
+		if (!in_bounds(point_to_add) ||
+				occupied_positions[point_to_add.x][point_to_add.y] != EMPTY)
+			is_blocked[i % 4] = true;
+
+		if (in_bounds(point_to_add) &&
+				occupied_positions[point_to_add.x][point_to_add.y] != this->color)
+			possible_moves.push_back(point_to_add);
+
+		all_blocked = is_blocked[0] && is_blocked[1] && is_blocked[2] && is_blocked[3];
+	}
+
+	return possible_moves;
 }
 
