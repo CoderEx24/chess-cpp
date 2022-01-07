5ab910db4dd9b0f2509209fd14bd0c36120412f8
CoderEx24 <coderex24@protonmail.com>
2022-01-07 02:18:41 +0200

    made Queen a friend of Bishop and Rook

--- include/core/bishop.hpp
+++ include/core/bishop.hpp
@@ -3,6 +3,10 @@
 
 #include "core/abstract_chess_piece.hpp"
 
+#ifdef QUEEN_EZ_IMP
+#include "core/queen.hpp"
+#endif
+
 class Bishop : public AbstractChessPiece
 {
 	public:
@@ -10,7 +14,9 @@ class Bishop : public AbstractChessPiece
 		Bishop(int x, int y, PieceColor c): AbstractChessPiece(x, y, c) {}
 
 		const std::vector<Position>& get_valid_positions(PieceColor **occupied_positions) override;
-		
+#ifdef QUEEN_EZ_IMP
+	friend class Queen;
+#endif	
 };
 
 #endif
