e269da8b473ea02879beb46f5ffb43def8148cd1
CoderEx24 <coderex24@protonmail.com>
2022-01-09 01:28:34 +0200

    removed Queen from friends

--- include/core/bishop.hpp
+++ include/core/bishop.hpp
@@ -3,10 +3,6 @@
 
 #include "core/abstract_chess_piece.hpp"
 
-#ifdef QUEEN_EZ_IMP
-#include "core/queen.hpp"
-#endif
-
 class Bishop : public AbstractChessPiece
 {
 	public:
@@ -14,9 +10,6 @@ class Bishop : public AbstractChessPiece
 		Bishop(int x, int y, PieceColor c): AbstractChessPiece(x, y, c) {}
 
 		const std::vector<Position>& get_valid_positions(PieceColor **occupied_positions) override;
-#ifdef QUEEN_EZ_IMP
-	friend class Queen;
-#endif	
 };
 
 #endif
